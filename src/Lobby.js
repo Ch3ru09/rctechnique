1ab23e42c4af781924d8d2a02f2ebfe956b72e30
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-05-09 23:41:21 -0400

    added colored backgrounds

--- src/Lobby.js
+++ src/Lobby.js
@@ -2,25 +2,23 @@ import React from 'react'
 
 export default class Lobby extends React.Component {
   constructor (props) {
-    super(props)
-    this.state = {
-
-    }
+    super(props);
+    this.state = {};
   }
 
   handleImageClick(page) {
-    this.props.changePage(page)
+    this.props.changePage(page);
   }
 
   render() {
     return (
       <>
-        <div className="container">
+        <div className="container bg-primary-200">
           <img src="smartphone.png" alt="phone" onClick={() => {this.handleImageClick("phones")}} /> {/* https://www.flaticon.com/free-icons/phone */}
           <img src="tablet.png" alt="tablet" onClick={() => {this.handleImageClick("tablets")}} /> {/* https://www.flaticon.com/free-icons/tablet */}
           <img src="laptop.png" alt="laptop" onClick={() => {this.handleImageClick("laptops")}} /> {/* https://www.flaticon.com/free-icons/laptop */}
         </div>
-        <div className="container">
+        <div className="container bg-dark-700">
           <img src="plant.png" alt="plant" onClick={() => {this.handleImageClick("plantPots")}} />
           <img src="sticker.png" alt="sticker" onClick={() => {this.handleImageClick("stickers")}} />
         </div>
