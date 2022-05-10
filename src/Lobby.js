33f38b2d415883f348c1e3eefcfc3100e2215d35
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-05-09 23:54:27 -0400

    changed img url to public_url

--- src/Lobby.js
+++ src/Lobby.js
@@ -14,13 +14,13 @@ export default class Lobby extends React.Component {
     return (
       <>
         <div className="container bg-primary-200">
-          <img src="smartphone.png" alt="phone" onClick={() => {this.handleImageClick("phones")}} /> {/* https://www.flaticon.com/free-icons/phone */}
-          <img src="tablet.png" alt="tablet" onClick={() => {this.handleImageClick("tablets")}} /> {/* https://www.flaticon.com/free-icons/tablet */}
-          <img src="laptop.png" alt="laptop" onClick={() => {this.handleImageClick("laptops")}} /> {/* https://www.flaticon.com/free-icons/laptop */}
+          <img src={process.env.PUBLIC_URL + "img/smartphone.png"} alt="phone" onClick={() => {this.handleImageClick("phones")}} /> {/* https://www.flaticon.com/free-icons/phone */}
+          <img src={process.env.PUBLIC_URL + "img/tablet.png"} alt="tablet" onClick={() => {this.handleImageClick("tablets")}} /> {/* https://www.flaticon.com/free-icons/tablet */}
+          <img src={process.env.PUBLIC_URL + "img/laptop.png"} alt="laptop" onClick={() => {this.handleImageClick("laptops")}} /> {/* https://www.flaticon.com/free-icons/laptop */}
         </div>
         <div className="container bg-dark-700">
-          <img src="plant.png" alt="plant" onClick={() => {this.handleImageClick("plantPots")}} />
-          <img src="sticker.png" alt="sticker" onClick={() => {this.handleImageClick("stickers")}} />
+          <img src={process.env.PUBLIC_URL + "img/plant.png"} alt="plant" onClick={() => {this.handleImageClick("plantPots")}} />
+          <img src={process.env.PUBLIC_URL + "img/sticker.png"} alt="sticker" onClick={() => {this.handleImageClick("stickers")}} />
         </div>
       </>
       
