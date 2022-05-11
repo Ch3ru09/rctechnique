28ab1242eb4a5f19a8479a79cada1b2d2c362673
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-05-11 18:33:22 -0400

    added text-shadow to title

--- src/Lobby.jsx
+++ src/Lobby.jsx
@@ -18,7 +18,7 @@ export default class Lobby extends React.Component {
           <h1 className="company-title">RC Technique</h1>
         </div>
         <div className="container bg-primary-200">
-          <div className="icon-box split">
+          <div className="icon-box">
             <div>
               <img 
               src={process.env.PUBLIC_URL + "img/smartphone.png"} 
@@ -28,9 +28,11 @@ export default class Lobby extends React.Component {
               style={{"--order": "1", "--times": "3"}} />
               {/* https://www.flaticon.com/free-icons/phone */}
             </div>
-            <div>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, ab veniam. Voluptate blanditiis perferendis ex molestiae corporis illo maxime provident laudantium, aliquid dolor, assumenda tempora omnis cupiditate temporibus dignissimos magni sunt aliquam esse itaque, id consequuntur veritatis possimus labore! Sed natus recusandae laboriosam incidunt excepturi vel consequatur tenetur, quaerat magnam aliquid facilis sint corrupti assumenda iusto deserunt, soluta atque! Esse dolor, eligendi odio excepturi architecto, cupiditate aliquam ratione accusamus asperiores harum, laudantium culpa a nihil temporibus ab dolorem omnis est. Impedit iste hic itaque enim excepturi id nesciunt, unde dolores commodi sapiente cupiditate quasi consequatur eius atque expedita deleniti quod?</p>
-            </div>
+            <section>
+              <div className="text">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, ab veniam. Voluptate blanditiis perferendis ex molestiae corporis illo maxime provident laudantium, aliquid dolor, assumenda tempora omnis cupiditate temporibus dignissimos magni sunt aliquam esse itaque, id consequuntur veritatis possimus labore! Sed natus recusandae laboriosam incidunt excepturi vel consequatur tenetur, quaerat magnam aliquid facilis sint corrupti assumenda iusto deserunt, soluta atque! Esse dolor, eligendi odio excepturi architecto, cupiditate aliquam ratione accusamus asperiores harum, laudantium culpa a nihil temporibus ab dolorem omnis est. Impedit iste hic itaque enim excepturi id nesciunt, unde dolores commodi sapiente cupiditate quasi consequatur eius atque expedita deleniti quod?</p>
+              </div>
+            </section>
           </div>
           <div className="icon-box">
             <img
