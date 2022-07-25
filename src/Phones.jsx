6857b4391825789941b01a049c65a61620147ff7
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-07-25 15:12:03 -0400

    added companies things

--- src/Phones.jsx
+++ src/Phones.jsx
@@ -13,6 +13,16 @@ export default class Phones extends React.Component {
 function OrbitSystem(props) {
   
   return (
-    <div className="orbit-system"></div>
+    <>
+      <div className="orbit-system"></div>
+      <div className="prices">
+        <h2>Price approximation</h2>
+        <div className="prices-container">
+          <div className="ios"></div>
+          <div className="android"></div>
+          <div className="others"></div>
+        </div>
+      </div>
+    </>
   )
 }
