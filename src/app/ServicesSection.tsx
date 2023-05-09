24aa6b10df84b6ad12413df9cf4445071c26a7e1
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2023-05-09 15:58:36 -0400

    added parts to the main page

---
+++ src/app/ServicesSection.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function ServicesSection() {
+  return (
+    <section className="bg-gray-100 py-16">
+      <div className="container mx-auto">
+        <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">Our Services</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <div className="mb-4">
+              <Image src="/service-1.jpg" alt="Service 1" width={300} height={200} className="rounded-md" />
+            </div>
+            <h3 className="text-xl font-bold mb-2">Screen Repair</h3>
+            <p className="text-gray-700 leading-relaxed mb-4">We can fix any cracked or damaged phone screen, whether it's an iPhone, Samsung, or any other make and model.</p>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Learn More</button>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <div className="mb-4">
+              <Image src="/service-2.jpg" alt="Service 2" width={300} height={200} className="rounded-md" />
+            </div>
+            <h3 className="text-xl font-bold mb-2">Battery Replacement</h3>
+            <p className="text-gray-700 leading-relaxed mb-4">Is your phone battery not holding a charge like it used to? We can replace it quickly and affordably.</p>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Learn More</button>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-6">
+            <div className="mb-4">
+              <Image src="/service-3.jpg" alt="Service 3" width={300} height={200} className="rounded-md" />
+            </div>
+            <h3 className="text-xl font-bold mb-2">Water Damage Repair</h3>
+            <p className="text-gray-700 leading-relaxed mb-4">Accidentally dropped your phone in the pool or spilled a drink on it? We can repair water damage and get your phone back up and running.</p>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Learn More</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
---
+++ src/app/TestimonialsSection.tsx
@@ -0,0 +1,70 @@
+export default function TestimonialsSection() {
+  const testimonials = [
+    {
+      id: 1,
+      name: "John Doe",
+      quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac gravida velit. Suspendisse potenti.",
+      image: "/images/testimonial-1.jpg",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      quote: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque pharetra semper nunc id pellentesque.",
+      image: "/images/testimonial-2.jpg",
+    },
+    {
+      id: 3,
+      name: "Bob Smith",
+      quote: "Proin at condimentum tellus. Nulla facilisi. Pellentesque euismod rhoncus leo, id efficitur augue congue vel.",
+      image: "/images/testimonial-3.jpg",
+    },
+  ];
+
+  return (
+    <div className="bg-gray-100 py-12">
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Testimonials</h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">What our customers are saying</p>
+        </div>
+
+        <div className="mt-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {testimonials.map((testimonial) => (
+              <TestimonialCard key={testimonial.id} testimonial={testimonial} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function TestimonialCard({ testimonial }: CardProps) {
+  const { name, quote, image } = testimonial;
+
+  return (
+    <div className="bg-white overflow-hidden shadow rounded-lg">
+      <div className="px-4 py-5 sm:p-6">
+        <div className="flex items-center mb-3">
+          <div className="flex-shrink-0">
+            <img className="h-10 w-10 rounded-full" src={image} alt={name} />
+          </div>
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">{name}</p>
+          </div>
+        </div>
+        <div className="mt-3 text-base text-gray-500">{quote}</div>
+      </div>
+    </div>
+  );
+}
+
+type CardProps = {
+  testimonial: {
+    name: string;
+    quote: string;
+    image: string;
+  };
+};
+
