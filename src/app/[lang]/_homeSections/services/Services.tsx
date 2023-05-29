a8ab454733690377a823cf8024721b307de2ad9e
Ch3ru09 <xliang0905@gmail.com>
2023-05-29 00:18:55 -0400

    fixed service title language not swapping

--- src/app/[lang]/_homeSections/services/Services.tsx
+++ src/app/[lang]/_homeSections/services/Services.tsx
@@ -8,7 +8,7 @@ export default function SectionServices({ dict }: { dict: Dict["services"] }) {
   return (
     <section className="bg-gray-100 py-16">
       <div className="container mx-auto">
-        <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">Our Main Services</h2>
+        <h2 className="text-3xl md:text-4xl font-bold mb-8 text-center">{dict.title}</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           <div className="bg-gray-50 shadow-lg rounded-lg p-6">
             <div className="mb-4">
