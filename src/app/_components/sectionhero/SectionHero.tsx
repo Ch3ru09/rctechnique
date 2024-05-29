4fbf1c79eb72696c6fbd0b872f831403b62d2922
Ch3ru09 <xliang0905@gmail.com>
2024-05-28 23:01:46 -0400

    added div to contain hero section text

--- src/app/_components/sectionhero/SectionHero.tsx
+++ src/app/_components/sectionhero/SectionHero.tsx
@@ -5,9 +5,11 @@ import styles from "./sectionhero.module.css";
 export default function SectionHero() {
   return (
     <main className={styles["section-hero"]}>
-      <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
-      <h2 className={styles["description"]}>Nous réparons </h2>
-      <Link href={"/"}>Commencer votre réparation</Link>
+      <div>
+        <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
+        <h2 className={styles["description"]}>Nous réparons </h2>
+        <Link href={"/"}>Commencer votre réparation</Link>
+      </div>
     </main>
   );
 }
