d5e4f67de71a45d9b6ba2cf6e4719cf2d2c49d85
Ch3ru09 <xliang0905@gmail.com>
2024-06-09 16:35:38 -0400

    added css to button

--- src/app/_components/sectionhero/SectionHero.tsx
+++ src/app/_components/sectionhero/SectionHero.tsx
@@ -5,11 +5,11 @@ import styles from "./sectionhero.module.css";
 export default function SectionHero() {
   return (
     <main className={styles["section-hero"]}>
-      <div>
-        <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
-        <h2 className={styles["description"]}>Nous réparons </h2>
-        <Link href={"/"}>Commencer votre réparation</Link>
-      </div>
+      <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
+      <h2 className={styles["description"]}>Nous réparons </h2>
+      <Link href={"/"} className={"button hover-effect"}>
+        Commencer votre réparation
+      </Link>
     </main>
   );
 }
