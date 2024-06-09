bbcaf78904d1e45be2b35911366384343a78d74e
Ch3ru09 <xliang0905@gmail.com>
2024-06-09 17:31:31 -0400

    added color palette to root

--- src/app/_components/sectionhero/SectionHero.tsx
+++ src/app/_components/sectionhero/SectionHero.tsx
@@ -4,12 +4,14 @@ import styles from "./sectionhero.module.css";
 
 export default function SectionHero() {
   return (
-    <main className={styles["section-hero"]}>
-      <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
-      <h2 className={styles["description"]}>Nous réparons </h2>
-      <Link href={"/"} className={"button hover-effect"}>
-        Commencer votre réparation
-      </Link>
-    </main>
+    <section className={styles["section-hero"]}>
+      <div className={styles["container"]}>
+        <h1 className={styles["title"]}>Réparez Votre Téléphone Rapidement</h1>
+        <h2 className={styles["description"]}>Nous réparons </h2>
+        <Link href={"/"} className={"button hover-effect"}>
+          Commencer votre réparation
+        </Link>
+      </div>
+    </section>
   );
 }
