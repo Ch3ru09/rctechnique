878c02136c19746f71cf28c26092ef3c9d360777
Ch3ru09 <xliang0905@gmail.com>
2024-06-10 22:24:13 -0400

    created new section

---
+++ src/app/_components/sectionservices/SectionServices.tsx
@@ -0,0 +1,13 @@
+import styles from "./sectionservices.module.css";
+
+export default function SectionServices() {
+  return (
+    <section>
+      <h1>Our Services</h1>
+      <div>service 1</div>
+      <div>service 2</div>
+      <div>service 3</div>
+      <div>service 4</div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import NavBar from "./_components/navbar/NavBar";
 import SectionHero from "./_components/sectionhero/SectionHero";
+import SectionServices from "./_components/sectionservices/SectionServices";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <SectionHero />
+      <div className={styles["intro"]}>
+        <NavBar />
+        <SectionHero />
+      </div>
+      <SectionServices />
     </>
   );
 }
